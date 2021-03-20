0df195330e7a626b13718fb6a96dedc8bb6143dd
Rex Jason Alobba <rex.alobba@vaultintel.com>
2021-03-20 23:23:42 +1100

    Add UI Tests

--- ProductList/ProductListUITests/ProductListUITests.swift
+++ ProductList/ProductListUITests/ProductListUITests.swift
@@ -8,35 +8,29 @@
 import XCTest
 
 class ProductListUITests: XCTestCase {
-
+    let app = XCUIApplication()
+  
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        XCUIDevice.shared.orientation = UIDeviceOrientation.portrait
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
 
-//    func testExample() throws {
-//        // UI tests must launch the application that they test.
-//        let app = XCUIApplication()
-//        app.launch()
-//
-//        // Use recording to get started writing UI tests.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-//
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTApplicationLaunchMetric()]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
+  func testProductListScreen() {
+    app.launch()
+    XCTAssertTrue(app.navigationBars["Commbank Products"].staticTexts["Commbank Products"].exists)
+  }
+  
+  func testProductListToProductDetail() {
+    app.launch()
+    let collectionViewsQuery = app.collectionViews
+    collectionViewsQuery/*@START_MENU_TOKEN@*/.staticTexts["Smart Access"]/*[[".cells.staticTexts[\"Smart Access\"]",".staticTexts[\"Smart Access\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+    
+    let commbankProductsButton = app.navigationBars["ProductList.ProductDetailView"].buttons["Commbank Products"]
+    XCTAssertTrue(commbankProductsButton.exists)
+    XCTAssertTrue(app.staticTexts["Smart Access"].exists)
+  }
 }
